7175f5b880c9d00d7ed6330adf55c61b86bc8f68
mondroidz <monnippon@gmail.com>
2019-12-10 22:21:44 -0300

    v0.02

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 
 const express = require('express');
 const app = require("./src/app");
+const PORT = process.env.PORT || 3002
 
 // app.use ('/', (req, res) => {
 //     res.send("testando");
 // })
 
-app.listen(3002, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     return console.log('Servidor não encontrado.');
   } 
     console.log('iniciou em http://localhost:3002')
-});
+})
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+}) 
